9dde35812b6377342b4a54ad61bab4b06fb5512b
Abdelkarim <tijaniabdelkarime@gmail.com>
2021-01-26 00:21:49 +0100

    Fixed Cros problem, and added search for products by Category

--- src/main/java/com/ecommerce/projet/dao/ProductCategoryRepository.java
+++ src/main/java/com/ecommerce/projet/dao/ProductCategoryRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/ecommerce/projet/entity/Product.java
+++ src/main/java/com/ecommerce/projet/entity/Product.java
@@ -7,7 +7,6 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
-
 @Entity
 @Table(name="product")
 @Data
